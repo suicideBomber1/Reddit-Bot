15d4f96d0cf713a3de1b975084c425472c581a3f
Yashwant Nagarjuna <ranbonagarjuna@gmail.com>
2018-06-01 12:52:19 +0530

    Testing

---
+++ config.py
@@ -0,0 +1,7 @@
+username = 'Bot_G'
+password = 'SAIRAM369'
+client_id = 'X3Ie9jxGC6Ww0w'
+client_secret = 'B_hCvDGwrJBtxCfrp9_J7nnOgEg'
+user_agent = 'praw_tut'
+
+# Never use user_agent as 'bot'
--- reddit-bot.py
+++ reddit-bot.py
@@ -1,5 +1,6 @@
 import praw
 import config
+import time
 
 
 def bot_login():
@@ -9,3 +10,17 @@ def bot_login():
                     username=config.username,
                     password=config.password)
     return r
+
+
+def bot_run(r):
+    for comment in r.subreddit('test').comments(limit=25):
+        if "dog" in comment.body:
+            print("Post with \"dog\" found! And it's id is " + comment.id)
+            comment.reply("Atleast now it is working")
+            print("Replied to comment " + comment.id)
+
+    time.sleep(10)
+
+
+r = bot_login()
+bot_run(r)
